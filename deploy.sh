7b3d82a3d1bc06dbd09a9d61b5dccb1344c24db3
rosstimo <rosstimo@isu.edu>
2022-06-28 15:48:40 -0600

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,30 @@
-rsync -avzlh  tim-fw/.config/shell/ .config/shell/
+#!/bin/sh
+
+## backup shell configs. Note: -n opton is dryrun
+
+if [[ -r $PWD/paths.csv ]]; then
+  input=$PWD/paths.csv
+else
+  echo "oops, cant find paths.csv!" && exit 1
+fi
+  while IFS="," read -r path filename remarks
+  do
+    src=$path/$filename
+    dst=$PWD/$HOSTNAME
+    if [[ -r "$src" ]]; then
+      echo "$src exists" 
+      if [[ -e $dst ]]; then
+        mkdir -pv $dst
+        mkdir -pv $dst/bkp
+      fi
+        rsync -vabhPn $src $dst/$filename # add n to optons here for dry run
+    else
+      echo "oops, can't find $src"
+    fi
+  done < $input
+  unset input
+  unset path
+  unset filename
+  unset remarks
+  unset src
+  unset dst
