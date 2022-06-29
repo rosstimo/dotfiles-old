a3c06dd64cf9ffc7632ddd1ed5137147bce573db
rosstimo <rosstimo@isu.edu>
2022-06-29 12:25:55 -0600

    fix file check, format output

--- grab.sh
+++ grab.sh
@@ -2,24 +2,29 @@
 
 ## backup shell configs. Note: -n opton is dryrun
 
-if [[ -r $PWD/paths.csv ]]; then
-  input=$PWD/paths.csv
-else
-  echo "oops, cant find paths.csv!" && exit 1
-fi
-  while IFS="," read -r path filename remarks
-  do
-    src=$path/$filename
-    dst=$PWD/$HOSTNAME
-    if [[ -r "$src" ]]; then
-      echo "$src exists" 
-      if [[ -e $dst ]]; then
+ if [[ -r $PWD/paths.csv ]]; then
+   input=$PWD/paths.csv
+ else
+   echo "oops, cant find paths.csv!" && exit 1
+ fi
+ echo
+   while IFS="," read -r path filename remarks
+   do
+     src=$path/$filename
+     dst=$PWD/$HOSTNAME
+     if [[ -r "$src" ]]; then
+       if [[ ! -e $dst ]]; then
+        echo "this may be the first run..."
+				echo "creating directory $dst and $dst/bkp" 
         mkdir -pv $dst
         mkdir -pv $dst/bkp
+				echo
       fi
-        rsync -vabhP $src $dst/$filename # add n to optons here for dry run
+      echo "backing up $src to $dst"
+      rsync -abhqP $src $dst/$filename # add n to optons here for dry run
     else
-      echo "oops, can't find $src"
+        # notify missing file, skip if csv header
+        [[ $path = "path" ]] || echo "oops, can't find $src"
     fi
   done < $input
   unset input
@@ -28,3 +33,5 @@ fi
   unset remarks
   unset src
   unset dst
+  echo
+	exit 0
